d9bcc1f040906d3be0c4becba777077b08712f37
bwight <bwight@sbrmarketing.com>
2013-01-15 14:31:56 -0600

    Update

--- src/vmod_sigauth.c
+++ src/vmod_sigauth.c
@@ -8,30 +8,18 @@
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
-  return (0);
+	return (0);
 }
 
 const char *
-vmod_sigstring(struct sess *sp, const char *name)
+vmod_sigstring(struct sess *sp, const char *name,...)
 {
-  char *p;
-  unsigned u, v;
+	va_list ap;
+	char *p;
 
-  //char *host = VRT_GetHdr(sp, HDR_REQ, "\030Host:");
-  //char *date = VRT_GetHdr(sp, HDR_REQ, "\030Date:");
+	va_start(ap, name);
+	p = VRT_String(sp->wrk->ws, NULL, name, ap);
+	va_end(ap);
 
-  u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
-  p = sp->wrk->ws->f;  /* Front of workspace area */
-  v = snprintf(p, u, "%s %s", "2013-01-15T00:00:00", "api.sbrfeeds.com");
-  v++;
-
-  if (v > u) {
-  /* No space, reset and leave */
-    WS_Release(sp->wrk->ws, 0);
-    return (NULL);
-  }
-
-  /* Update work space with what we've used */
-  WS_Release(sp->wrk->ws, v);
-  return (p);
+	return (p);
 }
