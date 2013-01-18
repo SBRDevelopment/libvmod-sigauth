328ac0202aafccc34acc71b26cb1f5d306fbfa3c
bwight <bwight@sbrmarketing.com>
2013-01-18 17:07:57 -0600

    Basic functionality ready for testing.

--- src/vmod_sigauth.c
+++ src/vmod_sigauth.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <syslog.h>
 
 /*
  * mhash.h has a habit of pulling in assert(). Let's hope it's a define,
@@ -23,6 +24,19 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 	return (0);
 }
 
+int
+sizeofhdr(const struct http *hp) {
+	CHECK_OBJ_NOTNULL(hp, HTTP_MAGIC);
+	return hp->nhd - HTTP_HDR_FIRST;
+}
+
+int
+comparehdr (const void * a, const void * b)
+{
+    /* Offset the headers to ignore the first length byte and compare the names */
+    return strcasecmp (*(const char **) a + 1, *(const char **) b + 1);
+}
+
 const char *
 hmac_sha1(struct sess *sp, const char *key, const char *msg)
 {
@@ -32,6 +46,9 @@ hmac_sha1(struct sess *sp, const char *key, const char *msg)
 	size_t blocksize = mhash_get_block_size(hash);
 	unsigned char mac[blocksize];
 	unsigned char *data;
+
+	AN(key);
+	CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
 
 	td = mhash_hmac_init(hash, (void *)key, strlen(key), mhash_get_hash_pblock(hash));
 	mhash(td, msg, strlen(msg));
@@ -49,14 +66,15 @@ hmac_sha1(struct sess *sp, const char *key, const char *msg)
 	return data;
 }
 
-long
-append_header_name(struct sess *sp, const struct http *hp, char *p, unsigned u) {
+char *
+get_header_name(struct sess *sp, const struct http *hp, unsigned u) {
 
-	unsigned l;
-	char *c;
+	char *c, *p;
+	size_t l;
 	txt hdr;
 
-	assert(hp);
+	CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+	CHECK_OBJ_NOTNULL(hp, HTTP_MAGIC);
 	Tcheck(hp->hd[u]);
 
 	hdr = hp->hd[u];
@@ -64,53 +82,164 @@ append_header_name(struct sess *sp, const struct http *hp, char *p, unsigned u) 
 	if (hdr.b == NULL)
 		return 0;
 
-	for(c = hdr.b; c < hdr.e; c++) {
+	// The ':' character indicates the end of the header name
+	// so loop through each character in the string and then
+	// add the header name to the buffer.
+	for(c = hdr.b, l = 0; c < hdr.e; c++, l++) {
 		if(c[0] == ':') {
-			l = c - hdr.b;
-			break;
+			p = WS_Alloc(sp->ws, l + 3); 		/* Allocate memory ( l + \0xx + ':' ) */
+			//syslog(LOG_INFO, "get_header_name| header %c%.*s:", l + 1, l, hdr.b);
+			sprintf(p, "%c%.*s:", l + 1, l, hdr.b);
+			return p;
 		}
 	}
 
-	assert(hdr.b[l] == ':');
-
-	return sprintf(p, "%.*s", l, hdr.b);
+	return NULL;
 }
 
 const char *
 get_headers(struct sess *sp, const struct http *hp) {
 
-	int u, v;
-	unsigned i;
-	char *p;
+	unsigned u, r, l, j, i;
+	unsigned h = sizeofhdr(hp);
+	const char *hdrl[h];
+	const char *p;
+	const char *pptr;
 
-	u = WS_Reserve(sp->wrk->ws, 0);
+	CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+	CHECK_OBJ_NOTNULL(hp, HTTP_MAGIC);
+
+	for (l = HTTP_HDR_FIRST, j = 0; l < hp->nhd; l++, j++) {
+		hdrl[j] = get_header_name(sp, hp, l);
+	}
+
+	qsort (hdrl, h, sizeof(const char *), comparehdr);
+
+	// Should be faster to just allocate all the memory and
+	// release it when done instead of trying to calculate how much
+	// we need to reserve.
+	r = WS_Reserve(sp->wrk->ws, 0);
 	p = sp->wrk->ws->f;
+	pptr = p;
 
-	for (i = HTTP_HDR_FIRST; i < hp->nhd; i++) {
-		v += append_header_name(sp, hp, p, i);
+	for (i = 0; i < h; i++) {
+		if (strcasecmp(hdrl[i], "\005date:") == 0 ||
+			strcasecmp(hdrl[i], "\005host:") == 0 ||
+			strncasecmp(hdrl[i]+1, "x-sbr", 5) == 0) {
+			pptr += sprintf(pptr, "%s %s\n", hdrl[i] + 1 /* skip length prefix */, VRT_GetHdr(sp, HDR_REQ, hdrl[i]));
+			//syslog(LOG_INFO, "get_headers| header %d: %s = %s\n", i, hdrl[i], VRT_GetHdr(sp, HDR_REQ, hdrl[i]));
+		}
 	}
-	v++;
 
-	if(v > u) {
-		// No space, release memory and leave
+	pptr++;
+	u = pptr - p;
+
+	/* Out of memory, run away!! */
+	if (u > r) {
 		WS_Release(sp->wrk->ws, 0);
-		return (NULL);
+		return NULL;
 	}
 
-	// Release unused memory
-	WS_Release(sp->wrk->ws, v);
-	return (p);
+	WS_Release(sp->wrk->ws, u);
+
+	return p;
+}
+
+int
+get_body(struct sess *sp, char**body, int *ocl) {
+
+	char *cl_ptr;
+	unsigned long cl;
+	int re, buf_size, rsize;
+	char buf[2048];
+
+	CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+
+	cl_ptr = VRT_GetHdr(sp, HDR_REQ, "\017Content-Length:");
+	if (!cl_ptr) return -2;
+
+	*ocl = cl = strtoul(cl_ptr, NULL, 10);
+	if (cl <= 0) return -2;
+
+	//syslog(LOG_INFO, "get_body| pipeline length %d", Tlen(sp->htc->pipeline));
+	if(sp->htc->pipeline.b != NULL && Tlen(sp->htc->pipeline) == cl) {
+		//syslog(LOG_INFO, "get_body| complete buffer %s", sp->htc->pipeline.b);
+		*body = sp->htc->pipeline.b;
+	} else {
+		int rxbuf_size = Tlen(sp->htc->rxbuf);
+
+		// Do ws work
+		int u = WS_Reserve(sp->wrk->ws, 0);
+		if( u < cl + rxbuf_size + 1) {
+			WS_Release(sp->wrk->ws, 0);
+			return -3;
+		}
+		*body = (char*)sp->wrk->ws->f;
+		memcpy(*body, sp->htc->rxbuf.b, rxbuf_size);
+		sp->htc->rxbuf.b = *body;
+		*body += rxbuf_size;
+		*body[0] = 0;
+		sp->htc->rxbuf.e = *body;
+		WS_Release(sp->wrk->ws, cl + rxbuf_size + 1);
+
+		// Read post data
+		re = 0;
+		while(cl) {
+			if(cl > sizeof(buf)) {
+				buf_size = sizeof(buf) - 1;
+			} else {
+				buf_size = cl;
+			}
+
+			//syslog(LOG_INFO, "get_body| reading %u bytes into buffer", buf_size);
+			rsize = HTC_Read(sp->wrk, sp->htc, buf, buf_size);
+			if (rsize <= 0) {
+				return -3;
+			}
+
+			cl -= rsize;
+
+			memcpy(*body + re, buf, buf_size);
+			re += rsize;
+		}
+
+		sp->htc->pipeline.b = *body;
+		sp->htc->pipeline.e = *body + *ocl;
+
+		//syslog(LOG_INFO, "get_body| complete buffer %s", *body);
+	}
+	return 1;
 }
 
 const char *
-vmod_sigstring(struct sess *sp,
+vmod_sigstring(struct sess *sp, const char *method, const char *uri, const char *secret){
 
-		const char *secret
-){
-	const char *p;
+	int cl;
+	char *body;
 
-	p = get_headers(sp, sp->http);
+	int ret = get_body(sp, &body, &cl);
+	const char *h = get_headers(sp, sp->http);
 
-	return (p);
-	//return hmac_sha1(sp, secret, buf);
+	int len = strlen(method) + 1 + strlen(uri) + 1 + strlen(h) + strlen(body);
+	char *o = WS_Alloc(sp->ws, len);
+
+	strcat(o, method);
+	strcat(o, "\n");
+	strcat(o, uri);
+	strcat(o, "\n");
+	strcat(o, h);
+	strcat(o, body);
+
+	const char *d = hmac_sha1(sp, secret, o);
+
+	//syslog(LOG_INFO, "vmod_sigstring| method %s", method);
+	//syslog(LOG_INFO, "vmod_sigstring| uri %s", uri);
+	//syslog(LOG_INFO, "vmod_sigstring| headers %s", h);
+	//syslog(LOG_INFO, "vmod_sigstring| body %s", body);
+
+	//syslog(LOG_INFO, "bmod_sigstring| string_to_sign %s", o);
+
+	//syslog(LOG_INFO, "vmod_sigstring| hmac-sha1 %s", d);
+
+	return (d);
 }
